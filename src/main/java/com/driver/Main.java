a460d0e4c06ecdee8c96b29e442f9028b09b170d
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-11 12:14:35 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setName("Pravin");
+        System.out.println(rw.getName());
+
+        // Error : Name has a private access in com.driver.RWOnly
+    }
 }
